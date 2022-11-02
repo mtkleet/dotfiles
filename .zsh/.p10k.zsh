864fdaff603bfd6ed1750f97ffcafc1ff0109e4e
mtkleet <16854775+mtkleet@users.noreply.github.com>
2022-11-02 02:01:08 +0000

    Update .p10k.zsh

--- .zsh/.p10k.zsh
+++ .zsh/.p10k.zsh
@@ -158,8 +158,8 @@
     # typeset -g POWERLEVEL9K_EMPTY_LINE_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='%{%}'
     # fi
 
-    # examplpes of segment serparator working with 'POWERLEVEL9K_MODE="nerdfont-complete"' configuration
-    # 
+    # Examples of unusual segment serparators compatible with 'POWERLEVEL9K_MODE="nerdfont-complete"' configuration:         
+    # https://unix.stackexchange.com/a/595816 <- Use this script to get list of all characters in a font
     # Separator between same-color segments on the left.
     typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%31F\u2571'
     # Separator between same-color segments on the right.
