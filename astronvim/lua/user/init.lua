5e70e3c1eed91ec3ac2fe5c7847c74e6f094a1ff
mtkleet <mateusz@e.email>
2022-12-09 02:17:06 +0100

    nvim bump

--- astronvim/lua/user/init.lua
+++ astronvim/lua/user/init.lua
@@ -9,7 +9,7 @@ local config = {
     skip_prompts = false,
     show_changelog = true,
     auto_reload = false,
-    auto_quit = false,
+    auto_quit = false
   },
 
   colorscheme = "neosolarized",
@@ -42,7 +42,7 @@ local config = {
       status_diagnostics_enabled = true,
       icons_enabled = true,
       ui_notifications_enabled = true,
-    },
+    }
   },
 
   header = {
@@ -60,7 +60,7 @@ local config = {
   },
   default_theme = {
     colors = {
-      fg = "#708284",
+      fg = "#abb2bf",
       bg = "#001E27",
     },
     highlights = function(hl) -- or a function that returns a new table of colors to set
@@ -80,6 +80,7 @@ local config = {
       dashboard = true,
       highlighturl = true,
       hop = false,
+      lightspeed = false,
       ["neo-tree"] = true,
       notify = true,
       ["nvim-tree"] = false,
@@ -90,7 +91,7 @@ local config = {
       treesitter = true,
       vimwiki = false,
       ["which-key"] = true,
-    },
+    }
   },
 
   diagnostics = { virtual_text = true, underline = true },
@@ -102,8 +103,8 @@ local config = {
     formatting = {
       format_on_save = {
         enabled = false,
-        allow_filetypes = { "cpp", "c", "rs" },
-        ignore_filetypes = {},
+        allow_filetypes = { "rust", "cpp", "c", "go" },
+        ignore_filetypes = {}
       },
       disabled = {},
       timeout_ms = 1000,
@@ -111,21 +112,8 @@ local config = {
       --		return true
       -- end
     },
-    mappings = { n = { --[[ ["<leader>lf"] = false -- disable formatting keymap ]] }, },
-    ["server-settings"] = { clangd = { capabilities = { offsetEncoding = "utf-8" } },
-      -- example for addings schemas to yamlls
-      -- yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
-      --   settings = {
-      --     yaml = {
-      --       schemas = {
-      --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-      --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-      --       },
-      --     },
-      --   },
-      -- },
-    },
+    mappings = { n = { --[[ ["<leader>lf"] = false --]] } },
+    ["server-settings"] = { clangd = { capabilities = { offsetEncoding = "utf-8" } } },
   },
   mappings = {
     n = {
@@ -175,16 +163,13 @@ local config = {
             display = { "NvimNotify" },
             borders = "single",
             display_options = { notification_timeout = 2500 },
-            interpreter_options = { Python3_original = { error_truncate = "long" }, },
+            interpreter_options = { Python3_original = { error_truncate = "long" } },
           })
         end,
       },
       ["rcarriga/nvim-notify"] = {
         event = "UIEnter",
-        config = function()
-          require("notify").setup({ background_colour = "#001E27" })
-          vim.notify = require("notify")
-        end,
+        config = function() require("notify").setup({ background_colour = "#001E27" }) vim.notify = require("notify") end
       },
       ["vladdoster/remember.nvim"] = { config = function() require("remember") end },
       ["max397574/better-escape.nvim"] = { disable = true },
@@ -200,7 +185,6 @@ local config = {
           astronvim.status.component.file_info(),
           astronvim.status.component.git_diff(),
           astronvim.status.component.diagnostics(),
-          astronvim.status.component.cmd_info(),
           astronvim.status.component.fill(),
           astronvim.status.component.macro_recording(),
           astronvim.status.component.fill(),
@@ -214,6 +198,8 @@ local config = {
           hl = { fg = "fg", bg = "none" },
           astronvim.status.component.breadcrumbs { icon = { hl = true }, padding = { left = 1 } },
           astronvim.status.component.fill(),
+          -- astronvim.status.component.git_diff(),
+          -- astronvim.status.component.diagnostics(),
         },
       }
     end,
@@ -221,23 +207,22 @@ local config = {
     ["null-ls"] = function(config)
       local null_ls = require "null-ls"
       config.sources = {
-        --	null_ls.builtins.formatting.prettier,
-        --	null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.diagnostics.zsh,
         null_ls.builtins.diagnostics.misspell,
         null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.hover.printenv,
       }
       return config
     end,
 
     treesitter = {},
-    ["mason-lspconfig"] = { ensure_installed = { "rust_analyzer", "clangd" } },
-    ["mason-null-ls"] = { ensure_installed = { "zsh", "shellcheck", "misspell" } },
+    ["mason-lspconfig"] = { ensure_installed = { "sumneko_lua", "rust_analyzer", "clangd", "pylsp" } },
+    ["mason-null-ls"] = { ensure_installed = { "printenv", "shellcheck", "misspell" } },
+    ["mason-nvim-dap"] = { --[[ ensure_installed = { "python" }, -- ]] },
   },
 
-  luasnip = { filetype_extend = { --[[javascript = { "javascriptreact" },]] }, vscode = { paths = {} }, },
+  luasnip = { filetype_extend = { javascript = { "javascriptreact" } }, vscode = { paths = {} } },
 
-  cmp = { source_priority = { nvim_lsp = 1000, luasnip = 750, buffer = 500, path = 250 } },
+  cmp = { source_priority = { nvim_lsp = 1000, luasnip = 750, buffer = 500, path = 50 } },
 
   ["which-key"] = { register = { n = { ["<leader>"] = { ["b"] = { name = "Buffer" }, }, }, }, },
 
