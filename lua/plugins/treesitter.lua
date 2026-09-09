9c70342d710b2e289ba6576080ddaf8e3687c43d
mtkleet <mateusz@e.email>
2026-09-09 15:31:17 +0200

    Update nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,41 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      treesitter = {
+        highlight = true,
+        indent = true,
+        auto_install = true,
+        ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    commit = false,
+    pin = false,
+    version = false,
+    build = ":TSUpdate",
+    config = function()
+      local ts = require "nvim-treesitter"
+      ts.setup()
+      ts.install {
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    commit = false,
+    pin = false,
+    version = false,
+  },
 }
